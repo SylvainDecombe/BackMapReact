4277ead77d580fd8544e3a73538ea4227fe5d07b
Guillaume-Beau <guillaume.beau12@gmail.com>
2020-09-30 16:29:18 +0200

    commentaire routercrud

--- routes/CrudRouter.js
+++ routes/CrudRouter.js
@@ -3,13 +3,14 @@ module.exports = app => {
 
     var router = require ("express").Router();
 
+    //creation utilisateur
     router.post("/", crud.create);
-
+    //MAJ profile
     router.get("/:id", crud.update);
-
+    //supression par id
     router.get("/:id", crud.delete);
-
+    //supprimer tous utilisateur
     router.get("/", crud.deleteAll);
-
+    
     app.use("/api/crud", router);
 };
