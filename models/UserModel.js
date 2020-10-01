f166c0390551a688bdb933db8bf5e3c1b90e74f5
Sylvain-Decombe <sylvain.decombe@etu.unilim.fr>
2020-10-01 14:12:03 +0200

    Model User

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,38 +2,42 @@ const { Schema, model } = require('mongoose');
 
 const UserModel = Schema({
 
-    firstname: {
-		type: String,
-		required: true
-	},
-	lastname: {
-		type: String,
-		required: true
-    },
-    password: {
+    prenom: {
         type: String,
         required: true
     },
-	email: {
-		type: String,
-		required: true
-	},
-	tel: {
-		type: String,
-		required: true
-	},
-	adresse: {
-		type: String,
-		required: true
+    nom: {
+        type: String,
+        required: true
+    },
+    motdepasse: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    tel: {
+        type: String,
+        required: true
+    },
+    adresse: {
+        type: String,
+        required: true
     },
     postal: {
-		type: Number,
-		required: true
+        type: Number,
+        required: true
     },
     ville: {
-		type: String,
-		required: true
-	}
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
 
 });
 
