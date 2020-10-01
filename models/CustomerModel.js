1cfcc708af450700b8a10fae8540158d932d1189
Sylvain-Decombe <sylvain.decombe@etu.unilim.fr>
2020-10-01 11:55:39 +0200

    Finish Models

--- models/CustomerModel.js
+++ models/CustomerModel.js
@@ -1,10 +1,30 @@
 const { Schema, model } = require('mongoose');
 
 const CustomerModel = Schema({
-	name: {
-		type: String,
-		required: true
-	}
+    prenom: {
+        type: String,
+        required: true
+    },
+    nom: {
+        type: String,
+        required: true
+    },
+    tel: {
+        type: String,
+        required: true
+    },
+    adresse: {
+        type: String,
+        required: true
+    },
+    postal: {
+        type: Number,
+        required: true
+    },
+    ville: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = model('Customer', CustomerModel);
--- models/ManufacturerModel.js
+++ models/ManufacturerModel.js
@@ -2,38 +2,38 @@ const { Schema, model } = require('mongoose');
 
 const ManufacturerModel = Schema({
 
-    firstname: {
-		type: String,
-		required: true
-	},
-	lastname: {
-		type: String,
-		required: true
-    },
-    password: {
+    nom: {
         type: String,
         required: true
     },
-	email: {
-		type: String,
-		required: true
-	},
-	tel: {
-		type: String,
-		required: true
-	},
-	adresse: {
-		type: String,
-		required: true
-	},
-	postal: {
-		type: Number,
-		required: true
-	},
-	ville: {
-		type: String,
-		required: true
-	}
+    email: {
+        type: String,
+        required: true
+    },
+    tel: {
+        type: String,
+        required: true
+    },
+    adresse: {
+        type: String,
+        required: true
+    },
+    postal: {
+        type: Number,
+        required: true
+    },
+    ville: {
+        type: String,
+        required: true
+    },
+    pays: {
+        type: String,
+        required: true
+    },
+    siret: {
+        type: String,
+        required: true
+    }
 
 });
 
